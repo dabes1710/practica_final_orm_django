b28b8a46a0c0517685953812fcef4ed2e3ab9628
dabes1710 <david.belmar@gmail.com>
2025-01-28 23:46:52 -0300

    Instala DB y migraciones

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Laboratorio, DirectorGeneral, Producto
 
-# Register your models here.
+@admin.register(Laboratorio)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad', 'pais')
+
+@admin.register(DirectorGeneral)
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'laboratorio', 'especialidad')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+
